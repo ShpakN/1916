4d10954fdb12ad1de9252028e5f87d93e145d9fb
ShpakN <121768458+ShpakN@users.noreply.github.com>
2024-03-31 12:24:43 +0300

    first commit

--- main.c
+++ main.c
@@ -8,13 +8,13 @@ matrix * createMatrixFromArray(const int *a,
 
                                int nRows, int nCols) {
 
-    matrix *m = getMemMatrix(nRows, nCols);
+    matrix m = getMemMatrix(nRows, nCols);
     int k = 0;
     for (int i = 0; i < nRows; i++)
         for (int j = 0; j < nCols; j++) {
-            m->values[i][j] = a[k++];
+            m.values[i][j] = a[k++];
         }
-    return m;
+    return &m;
 }
 
 int test_countZeroRows() {
@@ -31,7 +31,54 @@ int test_countZeroRows() {
     assert(test_countZeroRows(m, 5, 3) == 2);
     freeMemMatrices(m, 5);
 }
-
+int test_output() {
+    matrix *m = inputMatrices(
+            (matrix *) (int[]) {
+                    1, 1, 0,
+                    0, 0, 0,
+                    0, 0, 1,
+                    0, 0, 0,
+                    0, 1, 1,
+            },
+            5);
+    outputMatrix(*m);
+}
+int test_outputs() {
+    matrix *m = inputMatrices(
+            (matrix *) (int[]) {
+                    1, 1, 0,
+                    0, 0, 0,
+                    0, 0, 1,
+                    0, 0, 0,
+                    0, 1, 1,
+            },
+            5);
+    outputMatrices(m, 2);
+}
+int test_swapRows() {
+    matrix *m = createMatrixFromArray(
+            (int[]) {
+                    1, 1, 0,
+                    0, 0, 0,
+                    0, 0, 1,
+                    0, 0, 0,
+                    0, 1, 1,
+            },
+            5,2);
+    swapRows((matrix **) m, 2, 3);
+}
+int test_swapColums() {
+    matrix *m = createMatrixFromArray(
+            (int[]) {
+                    1, 1, 0,
+                    0, 0, 0,
+                    0, 0, 1,
+                    0, 0, 0,
+                    0, 1, 1,
+            },
+            5,2);
+    swapColumns((matrix **) m, 2, 3);
+}
 matrix *createArrayOfMatrixFromArray(const int *values, size_t nMatrices, size_t nRows, size_t nCols) {
     matrix *ms = getMemArrayOfMatrices(nMatrices, nRows, nCols);
     int l = 0;
@@ -45,9 +92,12 @@ matrix *createArrayOfMatrixFromArray(const int *values, size_t nMatrices, size_t
 
 void test() {
     test_countZeroRows();
+    test_output();
+    test_outputs();
+    test_swapRows();
+    test_swapColums();
 }
 
 int main() {
     test();
 }
-
